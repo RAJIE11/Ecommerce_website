9c2b3b6bfed5c5df37940db01c6aabbbade864d1
RAJIE11 <rajierajie465@gmail.com>
2023-09-03 23:42:36 +0530

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import '@/styles/globals.css'
 import { Layout } from '@/components'
+
+
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
