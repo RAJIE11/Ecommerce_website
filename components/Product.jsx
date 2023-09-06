9798be0b0b28d06ebbf0421a69b65e5bba1defb9
RAJIE11 <rajierajie465@gmail.com>
2023-09-06 16:06:06 +0530

    third commit

--- components/Product.jsx
+++ components/Product.jsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 
 import { urlFor } from '../lib/client';
 
-const Product = ({ product: { image, name, slug, price } }) => {
+const Product = ({ product: { image, name,slug, price } }) => {
   return (
     <div>
-      <Link href={`/product/${slug.current}`}>
+       <Link href={`/product/${slug.current}`}>
         <div className="product-card">
           <img 
             src={urlFor(image && image[0])}
