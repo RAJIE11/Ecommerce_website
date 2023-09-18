3128bcaccedab165a94b99d9dd771f912f43a1bc
RAJIE11 <rajierajie465@gmail.com>
2023-09-18 18:59:22 +0530

    final commit

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -8,7 +8,7 @@ const HeroBanner = ({heroBanner}) => {
     <div className='hero-banner-container'>
       <div>
         <p className='beats-solo'>{heroBanner.smallText}</p>
-        <h3>{heroBanner.midText}</h3>
+        <h3 className='midname'>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
        
         <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
         billing_address_collection: 'auto',
         
         shipping_options: [
-          { shipping_rate: 'shr_1NrMEnSHNbZkmbe5vAaDxWCn' },
+          {shipping_rate: 'shr_1NrMEnSHNbZkmbe5vAaDxWCn' },
           {shipping_rate : 'shr_1NrMGfSHNbZkmbe5HR0DxBUu'}
         ],
         line_items: req.body.map((item) => {
