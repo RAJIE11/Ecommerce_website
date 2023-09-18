ca1b450eac764c6091be492eae524d6803d4a351
RAJIE11 <rajierajie465@gmail.com>
2023-09-18 21:39:36 +0530

    ...

--- src/app/studio/[[...index]]/page.jsx
+++ src/app/studio/[[...index]]/page.jsx
@@ -5,8 +5,8 @@
  * All routes under your studio path is handled by this file using Next.js' catch-all routes:
  * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
  *
- * You can learn more about the next-sanity package here:
- * https://github.com/sanity-io/next-sanity
+ * You can learn more about the next-sanity package here:-
+ * https://github.com/sanity-io/next-sanity 
  */
 
 import { NextStudio } from 'next-sanity/studio'
